c9411fd7c49f20a7f8a6d5c58685e52037ef0303
Aaron Brinckerhoff <aaron.brinckerhoff@gmail.com>
2024-04-21 15:48:14 -0700

    add pages and routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
-
+import { Routes, Route } from 'react-router-dom'
+import { Signin, Signup, Dashboard, Account} from './components/pages/index'
 
 function App() {
   
   return (
-    <h1>Sim Stocks 2</h1>
+    <Routes>
+      <Route path='/' element={<Signin />} />
+      <Route path='/signup' element={<Signup />} />
+      <Route path='/signup' element={<Dashboard />} />
+      <Route path='/signup' element={<Account />} />
+    </Routes>
   )
 }
 
---
+++ src/components/pages/index.ts
@@ -0,0 +1,6 @@
+import Signin from './Signin'
+import Signup from './Signup'
+import Dashboard from './Dashboard'
+import Account from './Account'
+
+export { Signin, Signup, Dashboard, Account }
