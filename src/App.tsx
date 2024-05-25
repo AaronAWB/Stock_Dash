8dd985f677870a83586c224df2dfe5a81725a952
Aaron Brinckerhoff <aaron.brinckerhoff@gmail.com>
2024-05-25 12:54:59 -0700

    add protected routes to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom'
 import { Signin, Signup, Dashboard, Account} from './components/pages/index'
 import { AuthContextProvider } from './context/AuthContext'
+import ProtectedRoute from './components/protectedRoute'
 
 function App() {
   
@@ -9,8 +10,12 @@ function App() {
       <Routes>
         <Route path='/' element={<Signin />} />
         <Route path='/signup' element={<Signup />} />
-        <Route path='/dashboard' element={<Dashboard />} />
-        <Route path='/account' element={<Account />} />
+        <ProtectedRoute>
+          <Route path='/dashboard' element={<Dashboard />} />
+        </ProtectedRoute>
+        <ProtectedRoute>
+          <Route path='/account' element={<Account />} />
+        </ProtectedRoute>
       </Routes>
     </AuthContextProvider>
       
