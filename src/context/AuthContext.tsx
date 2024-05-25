ac51ee07f5c9423e40aa914b92efe69a533541ba
Aaron Brinckerhoff <aaron.brinckerhoff@gmail.com>
2024-05-25 12:50:47 -0700

    add remaining Firebase functions

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,10 +1,12 @@
-import { createContext, useContext } from 'react'
+import { createContext, useContext, useState, useEffect } from 'react'
 import {createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, onAuthStateChanged} from 'firebase/auth'
 import { auth } from '../firebase'
 
 const UserContext = createContext();
 
 export const AuthContextProvider = ({ children }) => {
+
+  const [user, setUser] = useState({});
 
   const createUser = (email: string, password: string) => {
     return createUserWithEmailAndPassword(auth, email, password)
@@ -14,8 +16,19 @@ export const AuthContextProvider = ({ children }) => {
     return signInWithEmailAndPassword(auth, email, password);
   }
 
+  const logOut = () => {
+    return signOut(auth)
+  }
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser)
+    })
+    return () => unsubscribe()
+  })
+
   return (
-    <UserContext.Provider value={{createUser, signIn}}>
+    <UserContext.Provider value={{createUser, signIn, logOut, user} }>
       {children}
     </UserContext.Provider>
   )
