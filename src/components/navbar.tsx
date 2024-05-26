090ed16776089c711e02c18f6568305fdf494a81
Aaron Brinckerhoff <aaron.brinckerhoff@gmail.com>
2024-05-26 15:09:40 -0700

    add refactored navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -5,43 +5,32 @@ import { Avatar, Dropdown, Navbar as FlowbiteNavbar } from "flowbite-react";
 export function Navbar({ userEmail }) {
 
   return (
-    <FlowbiteNavbar fluid rounded className='border-b shadow-sm'>
-      <div className="flex items-center w-full">
-        <FlowbiteNavbar.Collapse className="hidden md:flex">
-          <FlowbiteNavbar.Link href="#" active>
-            Home
-          </FlowbiteNavbar.Link>
-          <FlowbiteNavbar.Link href="/dashboard">Dashboard</FlowbiteNavbar.Link>
-          <FlowbiteNavbar.Link href="/search">Search</FlowbiteNavbar.Link>
-          <FlowbiteNavbar.Link href="/trades">Trade History</FlowbiteNavbar.Link>
-        </FlowbiteNavbar.Collapse>
-        <div className="ml-auto flex items-center mr-2">
-          <Dropdown
-            arrowIcon={false}
-            inline
-            label={
-              <Avatar alt="User settings" img="https://flowbite.com/docs/images/people/profile-picture-5.jpg" rounded />
-            }
-          >
-            <Dropdown.Header>
-                <span className="block text-sm">Aaron Brinckerhoff</span>
-                <span className="block truncate text-sm font-medium">{userEmail}</span>
-            </Dropdown.Header>
-            <Dropdown.Item as="a" href="/account">Account</Dropdown.Item>
-            <Dropdown.Item as="a" href="/settings">Settings</Dropdown.Item>
-            <div className="border-t my-1"></div>
-            <Dropdown.Item className=' mt-1'>Sign out</Dropdown.Item>
-          </Dropdown>
-          <FlowbiteNavbar.Toggle className="ml-2" />
-        </div>
+    <FlowbiteNavbar fluid className='border-b'>
+      <div className="flex md:order-2">
+        <Dropdown
+          arrowIcon={false}
+          inline
+          label={
+            <Avatar alt="User settings" img="https://flowbite.com/docs/images/people/profile-picture-5.jpg" rounded />
+          }
+        >
+          <Dropdown.Header>
+            <span className="block text-sm">Aaron Brinckerhoff</span>
+            <span className="block truncate text-sm font-medium">{userEmail}</span>
+          </Dropdown.Header>
+          <Dropdown.Item>Account</Dropdown.Item>
+          <Dropdown.Item>Settings</Dropdown.Item>
+          <Dropdown.Divider />
+          <Dropdown.Item>Sign out</Dropdown.Item>
+        </Dropdown>
+        <FlowbiteNavbar.Toggle />
       </div>
-      <FlowbiteNavbar.Collapse className="md:hidden">
+      <FlowbiteNavbar.Collapse>
         <FlowbiteNavbar.Link href="#" active>
-          Home
+          Dashboard
         </FlowbiteNavbar.Link>
-        <FlowbiteNavbar.Link href="/dashboard">Dashboard</FlowbiteNavbar.Link>
-        <FlowbiteNavbar.Link href="/search">Search</FlowbiteNavbar.Link>
-        <FlowbiteNavbar.Link href="/trades">Trade History</FlowbiteNavbar.Link>
+        <FlowbiteNavbar.Link href="#">Search</FlowbiteNavbar.Link>
+        <FlowbiteNavbar.Link href="#">Trade History</FlowbiteNavbar.Link>
       </FlowbiteNavbar.Collapse>
     </FlowbiteNavbar>
 
