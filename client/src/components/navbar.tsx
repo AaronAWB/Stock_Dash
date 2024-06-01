1e48470ec167d6c0576e2447d65c366b2598b66a
Aaron Brinckerhoff <aaron.brinckerhoff@gmail.com>
2024-06-01 11:40:21 -0700

    split into client and server and install Fastify

---
+++ client/src/components/navbar.tsx
@@ -0,0 +1,54 @@
+
+import { Avatar, Dropdown, Navbar as FlowbiteNavbar } from "flowbite-react";
+import { UserAuth } from '../context/AuthContext'
+import { useNavigate } from 'react-router-dom'
+
+
+export function Navbar({ userEmail }) {
+
+  const { logOut } = UserAuth()
+
+  const navigate = useNavigate()
+
+  const handleLogout = async () => {
+    try {
+      await logOut()
+      navigate('/')
+    } catch (error: any) {
+      console.error(error.message)
+    }
+  }
+
+  return (
+    <FlowbiteNavbar fluid className='border-b'>
+      <div className="flex md:order-2">
+        <Dropdown
+          arrowIcon={false}
+          inline
+          label={
+            <Avatar alt="User settings" img="https://flowbite.com/docs/images/people/profile-picture-5.jpg" rounded />
+          }
+        >
+          <Dropdown.Header>
+            <span className="block text-sm">Aaron Brinckerhoff</span>
+            <span className="block truncate text-sm font-medium">{userEmail}</span>
+          </Dropdown.Header>
+          <Dropdown.Item as="a" href="/account">Account</Dropdown.Item>
+          <Dropdown.Item as="a" href="/settings">Settings</Dropdown.Item>
+          <Dropdown.Divider />
+          <Dropdown.Item onClick={handleLogout}>Sign out</Dropdown.Item>
+        </Dropdown>
+        <FlowbiteNavbar.Toggle />
+      </div>
+      <FlowbiteNavbar.Collapse>
+        <FlowbiteNavbar.Link href="#" active>
+          Dashboard
+        </FlowbiteNavbar.Link>
+        <FlowbiteNavbar.Link href="#">Search</FlowbiteNavbar.Link>
+        <FlowbiteNavbar.Link href="#">Trade History</FlowbiteNavbar.Link>
+      </FlowbiteNavbar.Collapse>
+    </FlowbiteNavbar>
+
+  );
+}
+
