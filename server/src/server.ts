dff6e92cd46c7308c2255abd3dd061c935f44b61
Aaron Brinckerhoff <aaron.brinckerhoff@gmail.com>
2025-01-12 12:59:23 -0800

    install Prisma

--- server/src/server.ts
+++ server/src/server.ts
@@ -10,10 +10,14 @@ server.get('/', async (request, reply) => {
     return { hello: 'world'}
 })
 
+server.get('/api', async (request, reply) => {
+    return { message: "This is the API route!!" }
+})
+
 const start = async () => {
     try {
         await server.listen({ port })
-        console.log(`Server listening on ${port}`)
+        console.log(`*** [Server listening on ${port}] ***`)
     } catch (err) {
         server.log.error(err)
         process.exit(1)
