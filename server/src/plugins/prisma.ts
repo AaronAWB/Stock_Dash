dbaac284e381e184dbe996cf7f1a1e8b4883532f
Aaron Brinckerhoff <aaron.brinckerhoff@gmail.com>
2025-01-17 16:24:15 -0800

    refactor user table and add session table

--- server/src/plugins/prisma.ts
+++ server/src/plugins/prisma.ts
@@ -1,14 +1,17 @@
 import fp from 'fastify-plugin'
 import { PrismaClient } from '@prisma/client'
 
-const prismaPlugin = fp(async (fastify, opts) => {
+const prisma = new PrismaClient();
+
+const prismaPlugin = fp(async (fastify) => {
     const prisma = new PrismaClient();
 
     fastify.decorate('prisma', prisma)
 
-    fastify.addHook('onClose', async (instance) => {
+    fastify.addHook('onClose', async () => {
         await prisma.$disconnect()
     })
 })
 
+export { prisma }
 export default prismaPlugin;
--- server/src/routes/users.ts
+++ server/src/routes/users.ts
@@ -1,19 +1,20 @@
 import { FastifyPluginAsync } from 'fastify'
+import { prisma } from '../plugins/prisma'
 
 const userRoutes: FastifyPluginAsync = async (fastify) => {
     fastify.get('/users', async (request, reply) => {
-        const users = await fastify.prisma.user.findMany();
+        const users = await prisma.user.findMany();
         reply.send(users)
     })
 
     fastify.post('/users', async (request, reply) => {
         const {name, email, password} = request.body as { name: string, email: string, password: string};
-        const newUser = await fastify.prisma.user.create({
+        const newUser = await prisma.user.create({
             data: { name, email, password }
         })
         reply.send(newUser)
+        reply.status(201).send('User created.')
     })
-
 }
 
 export default userRoutes;
